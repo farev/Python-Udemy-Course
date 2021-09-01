b60d140527cdd948a032bef266c09932d66849c6
Fabian <50063514+farev@users.noreply.github.com>
2021-09-01 13:29:31 -0600

    Add files via upload

--- English_Dictionary/app1.py
+++ English_Dictionary/app1.py
@@ -1,1 +1,34 @@
+import json
+from difflib import get_close_matches
 
+data = json.load(open("data.json"))
+#data = json.load(open("dictionary.json"))
+
+def definition(Word):
+    Word = Word.lower()
+    if Word in data:
+        return data[Word]
+    elif Word.title() in data:
+        return data[Word.title()]
+    elif Word.upper() in data:
+        return data[Word.upper()]
+    elif len(get_close_matches(Word, data.keys(), cutoff=0.8)) > 0:
+        yn = input("Did you mean %s instead? Enter Y is yes, or N if no: " % get_close_matches(Word, data.keys())[0])
+        if yn == "Y" or "y":
+            return data[get_close_matches(Word, data.keys())[0]]
+        elif yn == "N" or "n":
+            return "The word doesn't exist. Please double check it"
+        else:
+            return "We didn't understand that"
+    else:
+        return "The word doesn't exist. Please double check it"
+
+word = input("Enter word: ")
+
+output = definition(word)
+
+if isinstance(output, list):
+    for item in output:
+        print(item)
+else:
+    print(output)
